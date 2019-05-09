b87871f03274a77620f6c629e7bf8f62ad9353f8
rainix <xyzhang_cn@outlook.com>
2019-05-09 15:53:06 +0800

    Install the webpack.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// import Vue from './vue.runtime.common.js'
+import Vue from 'vue'
 
 let app = new Vue({
     el: '#app',
     data: {
+        indexTitle: '房屋 婚介 租车 公证',
         msg: '这是Vue中的数据'
     }
 })
