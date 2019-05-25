29e60088cdcaaa89656c31e2101b3b781239a949
rainy <xyzhang_cn@outlook.com>
2019-05-25 17:27:55 +0800

    Finish the tik-tok video static layout.

--- src/components/tiktokvideo.vue
+++ src/components/tiktokvideo.vue
@@ -5,29 +5,51 @@
             <span class="centerTitle">Tik-Tok Video</span>
             <span class="line"></span>
         </div>
-        <div class="video_list">
-            <!-- <div class="videoBox" v-for="(item, index) in videoList">
-
-            </div> -->
+        <div class="video_list" :style="{height: contentHeight}" ref="mainContent">
+            <div class="videoBox" v-for="(item, index) in videoList" v-bind:key="index" 
+            v-bind:style="{left: 285 * (index % numInLine) + 'px', top: 450 * Math.floor(index / numInLine) + 'px'}"
+            @click="videoPlay">
+                <video :src="'/public/video/' + item.name"></video>
+            </div>
         </div>
-    </diV>
+        <div class="bottom_line">
+            <p>我是个有底线的网站！</p>
+        </div>
+    </div>
 </template>
 <script>
 export default {
     data: function(){
         return {
-            videoList: []
+            videoList: [],
+            contentHeight: '800px',
+            numInLine: 5,
+            playClass: 'pause'
         }
     },
     methods: {
+        videoPlay: function(){
+            
+        }
 
     },
     created: function(){
         ajax('GET', '/videoList', (xmlhttp) => {
-            console.log(xmlhttp.responseText);
+            this.$store.commit('setState', xmlhttp.responseText);
+            this.videoList = this.$store.state.videoList;
+            this.contentHeight = Math.ceil((this.videoList.length + 1) / this.numInLine) * 450 + 'px'
         }, (xmlhttp) => {
             console.error('Can\'t get the TikTok video list from server.');
         })
+    },
+    mounted: function(){
+        let that = this;
+        this.numInLine = Math.floor((this.$refs.mainContent.offsetWidth + 60) / 285);
+        // chrome不支持div标签的onresize事件，因此可以间接利用window对象的onresize事件
+        window.onresize = function(){
+            that.numInLine = Math.floor((that.$refs.mainContent.offsetWidth + 60) / 285);
+            that.contentHeight = Math.ceil((that.videoList.length + 1) / that.numInLine) * 450 + 'px';
+        };
     }
 }
 </script>
@@ -63,8 +85,38 @@ div.title span.line{
 div.video_list{
     position: relative;
     width: 90%;
-    height: 500px;
     margin: 0 auto;
-    background-color: yellow;
+    overflow: hidden;
+    /* background-color: yellow; */
+}
+
+div.video_list div.videoBox{
+    display: flex;
+    position: absolute;
+    align-items: center;
+    width: 225px;
+    height: 400px;
+    overflow: hidden;
+    background-color: black;
+}
+
+div.video_list div.videoBox video{
+    width: 100%;
+    height: auto;
+}
+
+div.bottom_line{
+    display: flex;
+    justify-content: center;
+    width: 90%;
+    margin: 0 auto;
+    border-top: 1px dotted #aaa;
+    /* background-color: green; */
+}
+
+div.bottom_line p{
+    height: 50px;
+    color: #aaa;
+    font-size: 16px;
 }
 </style>
